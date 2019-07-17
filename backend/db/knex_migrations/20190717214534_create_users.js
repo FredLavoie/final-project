1ebcf1ff8501dfe2bae171e52154b127d2f91b33
FredLavoie <flavoie3@gmail.com>
2019-07-17 22:23:16 +0000

    Complete backend setup minus db seeds

---
+++ backend/db/knex_migrations/20190717214534_create_users.js
@@ -0,0 +1,9 @@
+
+exports.up = function(knex) {
+  // See Dom's video here: https://www.youtube.com/watch?v=5cZwdT159WM&feature=youtu.be
+  // time: 1:12 :00
+};
+
+exports.down = function(knex) {
+  
+};
---
+++ backend/knexfile.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      SSL: process.env.DB_SSL,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './db/knex_migrations',
+    },
+    seeds: {
+      directory: './db/knex_seeds',
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
