7f06d8306aa2a0aad31b8a5b8a2057e543da232f
keefu <cyuplia@gmail.com>
2019-07-18 18:55:38 +0000

    Add deals migration

--- backend/db/knex_migrations/20190718001221_merchants.js
+++ backend/db/knex_migrations/20190718001221_merchants.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
-  
+  table.increments();
+  table.string('business_name');
+  table.string('email').unique();
+  table.string('password');
+  table.string('street_address');
+  table.string('city');
+  table.string('province');
+  table.string('postal_code');
+  table.biginteger('phone_number');
+  table.string('type_of_merchant');
+  table.timestamp(true, true)
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('merchants');
 };
---
+++ backend/db/knex_migrations/20190718174142_deals.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  table.increments();
+  table.integer('merchant_id').unsigned();
+  table.foreign('merchant_id').references('merchants.id');
+  table.integer('quantity_available');
+  table.string('image_path').alter();  
+  table.decimal('current_price');
+  table.timestamp(true, true)
+
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('deals');
+  
+};
