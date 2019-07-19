af9eb34f840d51f41f24f12d28c8b9a95677a6fb
saad hafa <saad.811@icloud.com>
2019-07-19 17:02:45 -0400

    react router set up

--- client/src/Component/Nav.js
+++ client/src/Component/Nav.js
@@ -8,20 +8,17 @@ export class Nav extends Component {
             <a href="#!" className="brand-logo">Food<span className="green">Cycle</span></a>
             <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
             <ul className="right hide-on-med-and-down menu-item">
-              <li><a href="sass.html">Sass</a></li>
-              <li><a href="badges.html">Components</a></li>
-              <li><a href="collapsible.html">Javascript</a></li>
-              <li><a href="mobile.html">Mobile</a></li>
+              <li><a href="sass.html">Deals</a></li>
+              <li><a href="collapsible.html">login</a></li>
+              <li><a href="mobile.html">Sign up</a></li>
             </ul>
           </div>
           <ul className="sidenav" id="mobile-demo">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">Javascript</a></li>
-          <li><a href="mobile.html">Mobile</a></li>
+          <li><a href="sass.html">Deals</a></li>
+          <li><a href="collapsible.html">login</a></li>
+          <li><a href="mobile.html">Sign up</a></li>
         </ul>
         </nav>
-      
         )
     }
 }
