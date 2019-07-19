226d704843a1fe759f04cd240504048a7778df59
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-07-19 21:31:44 +0000

    Add header and footer to merchant dashboard page

---
+++ client/src/pages/Merchant_dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Header from '../Component/Header';
+import Footer from '../Component/Footer';
+
+export class Merchant_dashboard extends Component {
+    render() {
+      return (
+        <div>
+          <Header />
+          <Footer />
+        </div>
+      )
+    }
+  }
+  
+  export default Merchant_dashboard
+  
