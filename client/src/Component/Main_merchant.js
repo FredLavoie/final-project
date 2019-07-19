561b9b733e4bc8eeb3b50e14fc607a586b135b5f
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-07-19 23:01:52 +0000

    Add text in main component for merchant

--- client/src/Component/Main_merchant.js
+++ client/src/Component/Main_merchant.js
@@ -4,7 +4,26 @@ export class Main extends Component {
     render() {
         return (
             <main> 
-                <h2>This is the page where you see the dashboard for the Merchant</h2>
+               <p>Here are you current deals:</p>
+               <p>Deal #1</p>
+               <p>item name: cronut</p>
+               <p>price: $2.00</p>
+               <p>quantity: 5</p>
+               <p>image: </p>
+
+                
+               <p>Deal #2</p>
+               <p>item name: cronut</p>
+               price: $2.00
+               quantity: 5
+               image: 
+
+                
+               Deal #3
+               item name: cronut
+               price: $2.00
+               quantity: 5
+               image: 
             </main>
         )
     }
