8fa22384fa39c4275c8421be680a3ad5d7352a19
keefu <cyuplia@gmail.com>
2019-07-20 15:40:33 +0000

    Add password minimum character length

--- client/src/Component/Registration.js
+++ client/src/Component/Registration.js
@@ -25,6 +25,12 @@ export class Registration extends Component {
       </div>
       <div class="row">
         <div class="input-field col s12">
+          <input id="password-confirm" type="password" class="validate"/>
+          <label for="password">Password Confirmation</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
           <input id="email" type="email" class="validate"/>
           <label for="email">Email</label>
         </div>
