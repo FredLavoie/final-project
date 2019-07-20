b8e6997b64f23e4a3571999b7c7647c73a521f00
keefu <cyuplia@gmail.com>
2019-07-20 01:19:01 +0000

    Create Login Page incomplete.

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from "materialize-css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 // import Component
 import Home from './pages/Home';
 import Deals from './pages/Deals';
+import Login from './pages/Login';
 
 class App extends Component {
 
@@ -22,6 +23,7 @@ class App extends Component {
       <Router>
         <Route exact path="/" component={Home} />
         <Route exact path="/deals" component={Deals} />
+        <Route exact path="/login" component={Login} />
       </Router>
       </div>
     );
