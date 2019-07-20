5d52e9e7c7a806e094eea904be39572f1dff9c90
saad hafa <saad.811@icloud.com>
2019-07-20 18:18:17 +0000

    fixed somme bug with className

--- client/src/Component/Loading.js
+++ client/src/Component/Loading.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react'
  class Loading extends Component {
   render() {
     return (
-      <div class="preloader-wrapper big active" style={{margin:'auto',position:'absolute',left:'45%',top:'200px'}}>
-      <div class="spinner-layer spinner-blue-only">
-        <div class="circle-clipper left">
-          <div class="circle"></div>
-        </div><div class="gap-patch">
-          <div class="circle"></div>
-        </div><div class="circle-clipper right">
-          <div class="circle"></div>
+      <div className="preloader-wrapper big active" style={{margin:'auto',position:'absolute',left:'45%',top:'200px'}}>
+      <div className="spinner-layer spinner-blue-only">
+        <div className="circle-clipper left">
+          <div className="circle"></div>
+        </div><div className="gap-patch">
+          <div className="circle"></div>
+        </div><div className="circle-clipper right">
+          <div className="circle"></div>
         </div>
       </div>
     </div>
