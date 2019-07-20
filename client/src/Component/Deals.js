c6cf802798d09027be4b267bbf0c2b8f4f8985e0
saad hafa <saad.811@icloud.com>
2019-07-20 18:23:38 -0400

    got the price showing up

--- client/src/Component/Deals.js
+++ client/src/Component/Deals.js
@@ -1,6 +1,10 @@
 
 import React, { Component } from 'react'
 
+
+const titleCardStyle = {position:'absolute',top:'0', left:'100px',height:'30px', padding:'3px', background:'red'}
+
+
 export class Deals extends Component {
   constructor(props){
     super(props)
@@ -8,17 +12,18 @@ export class Deals extends Component {
   render() {
   
     return (
-    <div className="col s12 m4" id={this.props.deal.id}>
+    <div className="col s12 m4" id={this.props.deal.id} style={{position:'relative'}}>
       <div className="card">
         <div className="card-image">
           <img style={{height:'15em',width:'100%'}} src={this.props.deal.image_path} />
-          <span className="card-title">{this.props.deal.name}</span>
+          <span className="card-title" style={titleCardStyle}>{this.props.deal.name}</span>
         </div>
         <div className="card-content">
-          <p>hello</p>
+          <strong>{this.props.deal.name}</strong>
+          <p>{this.props.deal.current_price} $</p>
         </div>
-        <div className="card-action">
-          <a href="#">add to cart</a>
+        <div className="card-action" style={{display:'flex', justifyContent:'center'}}>
+          <a  href="#">add to cart</a>
         </div>
       </div>
     </div>
--- client/src/Component/Header.js
+++ client/src/Component/Header.js
@@ -15,6 +15,4 @@ export class Header extends Component {
             )
     }
 }
-
-
 export default Header
