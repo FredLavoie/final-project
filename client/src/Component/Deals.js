f3b24ca5d4088d141f79514e7134596b3712683f
saad hafa <saad.811@icloud.com>
2019-07-20 21:16:00 +0000

    some merge conflict

--- client/src/Component/Deals.js
+++ client/src/Component/Deals.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react'
 
 export class Deals extends Component {
-  constructor(props){
-    super(props)
-  }
   render() {
-  
     return (
-    <div className="col s12 m4" id={this.props.deal.id}>
-      <div className="card">
-        <div className="card-image">
-          <img style={{height:'15em',width:'100%'}} src={this.props.deal.image_path} />
-          <span className="card-title">{this.props.deal.name}</span>
+      <main className="container"  style={{marginTop:'20px'}}>
+            <div class="row">
+    <div class="col s12 m3">
+      <div class="card">
+        <div class="card-image">
+          <img src="" />
+          <span class="card-title">Card Title</span>
         </div>
-        <div className="card-content">
-          <p>hello</p>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing</p>
         </div>
-        <div className="card-action">
-          <a href="#">add to cart</a>
+        <div class="card-action">
+          <a href="#">This is a link</a>
         </div>
       </div>
     </div>
+  </div>
+      </main>
     )
   }
 }
