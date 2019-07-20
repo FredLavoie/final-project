045a1afde24a65d0568a9e5a206d4d890ce86f00
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-07-20 15:25:40 +0000

    Add button and 100 vh to merchant page

--- client/src/Component/Main_merchant.js
+++ client/src/Component/Main_merchant.js
@@ -3,34 +3,46 @@ import React, { Component } from 'react'
 export class Main extends Component {
     render() {
         return (
-            <main> 
+            <main className="main-merchant"> 
+                <a class="waves-effect waves-light btn">Create New Deal</a>
                 <p>Your Deals</p>
                 <container className="title"> 
+                <container classsName="image-container">
+                    Image 
+                    </container>
+                <div>Product</div>
+                <div>Quantity</div>
+                <div>Price</div>
+                </container>
+                <container className="title"> 
+                <container classsName="image-container">
+                    <img className="responsive" src="https://images.deliveryhero.io/image/fd-ca/LH/e3ng-listing.jpg?width=800&height=584"/> 
+                    </container>
                 <div>White Chocolate</div>
                 <div>5</div>
                 <div>$2.00</div>
                 </container>
-               <p>Here are you current deals:</p>
-               <p>Deal #1</p>
-               <p>item name: cronut</p>
-               <p>price: $2.00</p>
-               <p>quantity: 5</p>
-               <p>image: </p>
+                <container className="title"> 
+                <container classsName="image-container">
+                    <img className="responsive" src="https://images.deliveryhero.io/image/fd-ca/LH/e3ng-listing.jpg?width=800&height=584"/> 
+                    </container>
+                <div>Most amazing Chocolate Ever</div>
+                <div>5</div>
+                <div>$2.00</div>
+                </container>
+                <container className="title"> 
+                <container classsName="image-container">
+                    <img className="responsive" src="https://images.deliveryhero.io/image/fd-ca/LH/e3ng-listing.jpg?width=800&height=584"/> 
+                    </container>
+                <div>Most amazing Chocolate Ever</div>
+                <div>5</div>
+                <div>$2.00</div>
+                </container>
 
-                
-               <p>Deal #2</p>
-               <p>item name: cronut</p>
-               price: $2.00
-               quantity: 5
-               image: 
-
-                
-               Deal #3
-               item name: cronut
-               price: $2.00
-               quantity: 5
-               image: 
+               
             </main>
+
+            
         )
     }
 }
