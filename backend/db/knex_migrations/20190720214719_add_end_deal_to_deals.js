e2d695f3d7df7c638ee9050b87fd36552ac1514c
saad hafa <saad.811@icloud.com>
2019-07-22 12:52:04 -0400

    fix some git conflict

--- backend/db/knex_migrations/20190720214719_add_end_deal_to_deals.js
+++ backend/db/knex_migrations/20190720214719_add_end_deal_to_deals.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex) {
-  knex.schema.table('deals', function(table){
-    table.datetime('end_date');  
+  return knex.schema.table('deals', function(table){
+    table.datetime('end_date');
   });
 }
 
 exports.down = function(knex) {
-  knex.schema.table('deals', function(table) {
+  return knex.schema.table('deals', function(table) {
     table.dropColumn('end_date');  
   });
 }
