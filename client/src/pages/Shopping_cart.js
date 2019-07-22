dcbb15ccb8d2a534c9421787dffaff3b1f2735f2
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-07-22 22:38:58 +0000

    Pass down state information to shopping cart page

--- client/src/pages/Shopping_cart.js
+++ client/src/pages/Shopping_cart.js
@@ -17,11 +17,11 @@ componentDidMount() {
 }
 
     render() {
-     
+     console.log("shooping", this.props.shoppingcart)
       return (
         <div>
           <Nav /> 
-          <MainShoppingCart />
+          <MainShoppingCart  />
           <Footer />
         </div>
       )
