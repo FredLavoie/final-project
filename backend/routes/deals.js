652cd13cd12a1d4190e013e6d6181f8925ceb043
FredLavoie <flavoie3@gmail.com>
2019-07-22 22:37:14 +0000

    Add merchant login

--- backend/routes/deals.js
+++ backend/routes/deals.js
@@ -37,25 +37,25 @@ router.get('/:id', function(req, res) {
 // [CREATE] a new deal
 router.post('/new', function(req, res) { // update with time remaining 
 
-  let dateEntered = dateToMilliseconds(req.body.exDate);
-  let timeEntered = req.body.exTime
-  let combined = 
+  // let dateEntered = dateToMilliseconds(req.body.exDate);
+  // let timeEntered = req.body.exTime
+  // let combined = 
   
-  let dealObject = {};
-  dealObject.merchant_id = req.body.merchant_id;
-  dealObject.name = req.body.name;
-  dealObject.description = req.body.description;
-  dealObject.quantity_available = req.body.quantity_available;
-  dealObject.image_path = req.body.image_path;
-  dealObject.current_price = req.body.current_price;
+  // //  let dealObject = {};
+  // dealObject.merchant_id = req.body.merchant_id;
+  // dealObject.name = req.body.name;
+  // dealObject.description = req.body.description;
+  // dealObject.quantity_available = req.body.quantity_available;
+  // dealObject.image_path = req.body.image_path;
+  // dealObject.current_price = req.body.current_price;
   // dealObject.expiryDate = combined;
   
-  knex
-    .insert(dealObject)
-    .into('deals')
-    .then( function() {
-      res.redirect('/');
-    });
+  // knex
+  //   .insert(dealObject)
+  //   .into('deals')
+  //   .then( function() {
+  //     res.redirect('/');
+  //   });
 
 });
 
--- backend/routes/merchants.js
+++ backend/routes/merchants.js
@@ -6,32 +6,94 @@ const ENV         		= process.env.ENV || "development";
 const knex            = require('knex')(knexConfig[ENV]);
 const express 				= require('express');
 const router 					= express.Router();
+const jwt             = require('jsonwebtoken');
+const auth            = require('../auth/auth');
 
 //************************************** ROUTES ***************************************/
 //*************************************************************************************/
 
 // [GET] merchant information
-router.get('/:merchant_id', function(req, res) {
+router.get('/current_merchant', auth, function(req, res) { 
   knex
     .select("*")
     .from("merchants")
-    .where("merchants.id", req.params.merchant_id)
+    .where("merchants.id", req.merchant_id)
     .then((data) => {
       res.json(data);
     });
-
 });
 
 // [GET] merchant deals
-router.get('/:merchant_id/dashboard', function(req, res) {
+router.get('/deals', auth, function(req, res) {
   knex
     .select("*")
     .from("deals")
-    .where("deals.merchant_id", req.params.merchant_id)
+    .where("deals.merchant_id", req.merchant_id)
     .then((data) => {
       res.json(data);
     });
+});
 
+// [LOGIN] get login page
+router.post('/login', function(req, res) {
+  const { email, password } = req.body;
+  console.log('This is the body: ',req.body);
+  
+  knex
+    .select("*")
+    .from("merchants")
+    .where("merchants.email", email)
+    .then(([ merchant ]) => {
+      console.log('Merchant post knex: ', merchant);
+      if(merchant.password === password) {
+        console.log('before signing token. Passwords match');
+        
+        jwt.sign(
+          { merchant_id: merchant.id },
+          process.env.JWT_SECRET,
+          { expiresIn: 60*60*24 }, (err, token) => {
+            if (err) {
+              console.log(err);
+            } else {
+              res.json({ token: token, merchant_id: merchant.id });
+            }
+          });
+      }
+    });
+});
+
+// [REGISTER]
+router.post('/register', function(req, res) {
+  let newUserObj = {};
+  newUserObj.business_name = req.body.business_name;
+  newUserObj.email = req.body.email;
+  newUserObj.password = req.body.password;
+  newUserObj.street_address = req.body.street_address;
+  newUserObj.city = req.body.city;
+  newUserObj.province = req.body.province;
+  newUserObj.postal_code = req.body.postal_code;
+  newUserObj.phone_number = req.body.phone_number;
+  newUserObj.type_of_merchant = req.body.type_of_merchant;
+  newUserObj.latitude = req.body.latitude;
+  newUserObj.longitude = req.body.longitude;
+
+  knex
+    .insert(newUserObj)
+    .into('merchants')
+    .returning('id')
+    .then( function([merchant_id]) {
+      jwt.sign(
+        { merchant_id: merchant_id },
+        process.env.JWT_SECRET,
+        { expiresIn: 60*60*24 }, (err, token) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.status(201).json({ token: token, merchant_id: merchant_id });
+          }
+        });
+    });
+  
 });
 
 
