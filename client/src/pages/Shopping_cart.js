80d7b16a83ac42026611bf15127dc15e740e5704
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-07-23 13:51:54 +0000

    Remove console.log

--- client/src/pages/Shopping_cart.js
+++ client/src/pages/Shopping_cart.js
@@ -2,18 +2,11 @@ import React, { Component } from 'react'
 import Nav from '../Component/Nav';
 import Footer from '../Component/Footer';
 import MainShoppingCart from '../Component/Main-shopping-cart'
-import M from "materialize-css";
 
 export class ShoppingCart extends Component {
   constructor(props){
     super(props)
-    this.state = {
-     }
 
-}
-
-componentDidMount() {
-  
 }
 
     render() {
