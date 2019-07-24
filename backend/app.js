7795fa521b481a5295278c14540e883d9fe5f840
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-07-24 18:11:50 -0400

    Add constants for stripe in backend

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,11 @@ const knexConfig      = require('./knexfile');
 const ENV         		= "development";
 const knex            = require('knex')(knexConfig[ENV]);
 const jwt             = require('jsonwebtoken');
+const configureStripe = require('stripe');
+const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
+    ? 'sk_live_MY_SECRET_KEY'
+    : 'sk_test_MY_SECRET_KEY';
+const stripe = configureStripe(STRIPE_SECRET_KEY);
 
 
 //********************************* EXTERNAL ROUTES ***********************************/
