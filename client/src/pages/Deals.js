d725e0fcc885c2624b419403e29b54601dfe23eb
keefu <cyuplia@gmail.com>
2019-07-25 17:15:43 -0400

    Display order deals by merchant it still need some love

--- client/src/pages/Deals.js
+++ client/src/pages/Deals.js
@@ -9,21 +9,29 @@ class Deals extends Component {
   render() {
 
 
-  let merchantDeal = _(this.props.deals).groupBy(deal => deal.business_name).map( (value, key)=> {
+  let merchantDealArr = _(this.props.deals).groupBy(deal => deal.business_name).map( (value, key)=> {
     return {
       business_name: key, deals: value
     }
   }).value()
 
-  console.log("Value of WHATEVER:",merchantDeal)
+  console.log("Value of merchantDealArr:",merchantDealArr)
 
-    const deals = this.props.deals.map((deal, index) => <DealsComponent key={index} deal={deal} add={this.props.add}/> );
+const merchantDeal = merchantDealArr.map(merchant => {
+  const deals = merchant.deals.map((deal, index) => <DealsComponent key={index} deal={deal} add={this.props.add}/> );
+    return (
+      <div>
+        <h2 className="merchantname">{merchant.business_name}</h2><div className="container">{deals}</div>
+      </div>
+    )
+  });
+    
     return (
       <div style={{minHeight: "100%", position: "relative"}}>
         <Nav/>
         <main className="container"  style={{marginTop:'20px', bottom: "20px"}}>
           <div className="row">
-            {this.props.isready ? deals : <Loading /> }
+            {this.props.isready ? merchantDeal : <Loading /> }
           </div>
         </main>
       </div>
