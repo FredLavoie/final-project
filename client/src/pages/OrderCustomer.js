d3f113dd75c368f00ba50a772edde297e69f2fc5
saad hafa <saad.811@icloud.com>
2019-07-26 12:58:38 -0400

    add confetti js

--- client/src/pages/OrderCustomer.js
+++ client/src/pages/OrderCustomer.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import Nav from '../Component/Nav';
-import Footer from '../Component/Footer';
+//import useWindowSize from 'react-use/lib/useWindowSize';
+import Confetti from 'react-confetti';
 
 
 export class MerchantDashboard extends Component {
-  constructor(props){
-    super(props);
-    
-  }
-  
+state = {
+  confetti: true
+}
+
+componentDidMount(){
+  setTimeout(() => {
+    this.setState({confetti: false})
+  },5000)
+}
+
   render() {
-  
+    //const { width, height } = useWindowSize()
     return (
       <div>
         <Nav /> 
-        <main className="main-page">
-        <p>Thank you for your order!</p>
+        <main className="main-page container" style={{textAlign: 'center'}}>
+        <p>Thank you for your order! <span style={{color: 'green'}}>{localStorage.getItem('username')}</span></p>
+          { this.state.confetti ? <Confetti width={900} height={900} /> : ''}
         </main>
       </div>
     );
