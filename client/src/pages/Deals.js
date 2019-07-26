301a5011306b08a099c5ae2f1bdb196f6a4461cb
keefu <cyuplia@gmail.com>
2019-07-26 10:52:29 -0400

    Carousel feature

--- client/src/pages/Deals.js
+++ client/src/pages/Deals.js
@@ -9,46 +9,49 @@ class Deals extends Component {
   state = {toggle : false}
 
   render() {
+    const dealsD = this.props.deals.map(deal => <DealsComponent key={deal.id} deal={deal} add={this.props.add} customClass="m4"/> );
     if(this.state.toggle){
-      return( <div> 
+      return( 
+      <div style={{minHeight: "100%", position: "relative"}}>
+      <div> 
         <Nav/>
         <p>
         <label>
-          <input name="group1" type="radio" onClick={() => this.setState({toggle:true})} />
+          <input name="group1" type="radio" onClick={() => this.setState({toggle:false})} />
           <span>Sort By Merchant</span>
         </label>
       </p>
       <p>
         <label>
-          <input name="group1" type="radio" onClick={() => this.setState({toggle:false})}/>
+          <input name="group1" type="radio" onClick={() => this.setState({toggle:true})} checked/>
           <span>Sort By Date</span>
         </label>
       </p>
-      {this.props.isready ? <DealMerchant deals={this.props.deals} add={this.props.add}/> : <Loading /> }
+      <main className="container"  style={{marginTop:'20px', bottom: "20px"}}>
+      <div className="row">
+      {this.props.isready ? dealsD : <Loading /> }
+      </div>
+        </main>
+      </div>
       </div>
       )
     }
-    const deals = this.props.deals.map(deal => <DealsComponent key={deal.id} deal={deal} add={this.props.add} customClass="m4"/> );
     return (
       <div style={{minHeight: "100%", position: "relative"}}>
         <Nav/>
         <p>
         <label>
-          <input name="group1" type="radio" onClick={() => this.setState({toggle:true})} />
+          <input name="group1" type="radio" onClick={() => this.setState({toggle:false})} checked/>
           <span>Sort By Merchant</span>
         </label>
       </p>
       <p>
         <label>
-          <input name="group1" type="radio" onClick={() => this.setState({toggle:false})}/>
+          <input name="group1" type="radio" onClick={() => this.setState({toggle:true})}/>
           <span>Sort By Date</span>
         </label>
       </p>
-        <main className="container"  style={{marginTop:'20px', bottom: "20px"}}>
-          <div className="row">
-            {this.props.isready ? deals : <Loading /> }
-          </div>
-        </main>
+        {this.props.isready ? <DealMerchant deals={this.props.deals} add={this.props.add}/> : <Loading /> }
       </div>
     );
   }
